f8400ec8675f768cd94f7f4ac6f6e5763e1f0e3e
h.ezzati <ezzati.h77@gmail.com>
2021-06-08 23:26:28 +0430

    update

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -3,3 +3,8 @@ function blink_text() {
     $('#top-banner-name').fadeIn(600);
 }
 setInterval(blink_text, 1500);
+function blink_text2() {
+    $('.sticky-note-val').fadeOut(600);
+    $('.sticky-note-val').fadeIn(600);
+}
+setInterval(blink_text2, 1500);
